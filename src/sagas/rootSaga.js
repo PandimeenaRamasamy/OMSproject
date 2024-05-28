2c028f046feb0d9c36924cc1866e9aa45930fb24
PandimeenaRamasamy <in.pandimeena.ramasamy@magilhub.com>
2024-05-28 15:57:20 +0530

    rootsaga

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -2,9 +2,11 @@
 
 import { all } from 'redux-saga/effects';
 import { watchPostData } from '../sagas/counterSaga';
+import { watchgetData } from '../sagas/counterSaga';
 
 export default function* rootSaga() {
   yield all([
     watchPostData(),
+    watchgetData()
   ]);
 }
