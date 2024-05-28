ad637833cac3c94e00ce354ab91b41b5b3be7733
Ankit <ankitkhurana519@gmail.com>
2024-05-28 15:02:53 +0530

    Reducer of onboarding added

--- src/Reducers/postDataReducer.js
+++ src/Reducers/postDataReducer.js
@@ -1,6 +1,7 @@
 // src/reducers/postDataReducer.js
 
-import { POST_DATA_REQUEST, POST_DATA_SUCCESS, POST_DATA_FAILURE } from '../Actions/PostDataAction';
+import { POST_DATA_REQUEST,POST_DATA_SUCCESS,POST_DATA_FAILURE,POST_ONBOARDING_DATA_REQUEST,POST_ONBOARDING_DATA_SUCCESS,POST_ONBOARDING_DATA_FAILURE,
+} from "../Actions/PostDataAction";
 
 const initialState = {
   loading: false,
@@ -15,6 +16,12 @@ const postDataReducer = (state = initialState, action) => {
     case POST_DATA_SUCCESS:
       return { ...state, loading: false, data: action.payload };
     case POST_DATA_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+    case POST_ONBOARDING_DATA_REQUEST:
+      return { ...state, loading: true, error: null };
+    case POST_ONBOARDING_DATA_SUCCESS:
+      return { ...state, loading: false, data: action.payload };
+    case POST_ONBOARDING_DATA_FAILURE:
       return { ...state, loading: false, error: action.payload };
     default:
       return state;
