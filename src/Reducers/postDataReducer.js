996a0444831b5dfcca932d232b4e6d63092843c3
Ankit <ankitkhurana519@gmail.com>
2024-05-28 16:07:06 +0530

    gegw

--- src/Reducers/postDataReducer.js
+++ src/Reducers/postDataReducer.js
@@ -1,7 +1,9 @@
 // src/reducers/postDataReducer.js
 
-import { POST_DATA_REQUEST, POST_DATA_SUCCESS, POST_DATA_FAILURE } from '../Actions/PostDataAction';
-import { GET_DATA_REQUEST, GET_DATA_SUCCESS, GET_DATA_FAILURE } from '../Actions/PostDataAction';
+import {
+  POST_DATA_REQUEST,POST_DATA_SUCCESS,POST_DATA_FAILURE,} from "../Actions/PostDataAction";
+import {GET_DATA_REQUEST,GET_DATA_SUCCESS, GET_DATA_FAILURE,} from "../Actions/PostDataAction";
+import { POST_ONBOARDING_DATA_REQUEST,POST_ONBOARDING_DATA_SUCCESS,POST_ONBOARDING_DATA_FAILURE,} from "../Actions/PostDataAction";
 const initialState = {
   loading: false,
   data: null,
@@ -17,16 +19,21 @@ const postDataReducer = (state = initialState, action) => {
     case POST_DATA_FAILURE:
       return { ...state, loading: false, error: action.payload };
     case GET_DATA_REQUEST:
-      return { ...state, loading: true, error: null};
+      return { ...state, loading: true, error: null };
     case GET_DATA_SUCCESS:
       return { ...state, loading: false, data: action.payload };
-    case  GET_DATA_FAILURE:
+    case GET_DATA_FAILURE:
       return { ...state, loading: false, error: action.payload };
-    default:
+
+    case POST_ONBOARDING_DATA_REQUEST:
+      return { ...state, loading: true, error: null };
+    case POST_ONBOARDING_DATA_SUCCESS:
+      return { ...state, loading: false, data: action.payload };
+    case POST_ONBOARDING_DATA_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+      default:
       return state;
   }
 };
-
-
 
 export default postDataReducer;
