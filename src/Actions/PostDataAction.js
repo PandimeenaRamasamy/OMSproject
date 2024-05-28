f1f2ecb3599ef242babd61e466bf84f65c6a978b
Ankit <ankitkhurana519@gmail.com>
2024-05-28 14:56:35 +0530

    onBoarding Action Added

--- src/Actions/PostDataAction.js
+++ src/Actions/PostDataAction.js
@@ -2,6 +2,9 @@
 export const POST_DATA_REQUEST = 'POST_DATA_REQUEST';
 export const POST_DATA_SUCCESS = 'POST_DATA_SUCCESS';
 export const POST_DATA_FAILURE = 'POST_DATA_FAILURE';
+export const POST_ONBOARDING_DATA_REQUEST = 'POST_ONBOARDING_DATA_REQUEST';
+export const  POST_ONBOARDING_DATA_SUCCESS="POST_ONBOARDING_DATA_SUCCESS";
+export const POST_ONBOARDING_DATA_FAILURE="POST_ONBOARDING_DATA_FAILURE"
 
 export const postDataRequest = (data) => ({
   type: POST_DATA_REQUEST,
@@ -17,3 +20,27 @@ export const postDataFailure = (error) => ({
   type: POST_DATA_FAILURE,
   payload: error,
 });
+
+export const postOnBoardingDataRequest=(data)=>{
+  return{
+    type:POST_ONBOARDING_DATA_REQUEST,
+    payload:data
+  }
+  
+}
+
+export const postOnBoardingDataSuccess=(response)=>{
+  return{
+    type:POST_ONBOARDING_DATA_SUCCESS,
+    payload:response
+  }
+  
+}
+
+export const postOnBoardingDataFailure=(error)=>{
+  return{
+    type:POST_ONBOARDING_DATA_FAILURE,
+    payload:error
+  }
+  
+}
