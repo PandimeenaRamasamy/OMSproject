51a3d57a2aaf9c5607e54df76caa3e1bd76e8e7b
AditKhurana <85402636+AditKhurana14@users.noreply.github.com>
2024-06-19 15:40:57 +0530

    as

--- src/Components/Onboarding/Fssai/Fssai.js
+++ src/Components/Onboarding/Fssai/Fssai.js
@@ -188,7 +188,7 @@ const Fssai = forwardRef((props, ref) => {
                           borderColor: fssaierrors.expirationDate ? "red" : "#B3B3B3",
                         }}
                       />
-                      {fssaierrors.expirationDate && <div className="error">{fssaierrors.expirationDate}</div>}
+                      {fssaierrors.expirationDate && <div className="error_FSSAI">{fssaierrors.expirationDate}</div>}
                     </div>
                     <div
                       style={{ display: "flex", flexDirection: "column" }}
@@ -213,7 +213,7 @@ const Fssai = forwardRef((props, ref) => {
                           borderColor: fssaierrors.registerNumber ? "red" : "#B3B3B3",
                         }}
                       />
-                      {fssaierrors.registerNumber && <div className="error">{fssaierrors.registerNumber}</div>}
+                      {fssaierrors.registerNumber && <div className="error_FSSAI">{fssaierrors.registerNumber}</div>}
                     </div>
                   </div>
 
@@ -274,7 +274,7 @@ const Fssai = forwardRef((props, ref) => {
                         )}
                       </div>
                     </div>
-                    {fssaierrors.documents && <div className="error">{fssaierrors.documents}</div>}
+                    {fssaierrors.documents && <div className="error_FSSAI">{fssaierrors.documents}</div>}
 
                   </div>
                 </>
