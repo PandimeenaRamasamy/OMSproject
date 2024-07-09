b08fc661c60ca9242a2083d523e11cd24bc9e87e
Ankit <ankitkhurana519@gmail.com>
2024-07-09 13:10:15 +0530

    www

--- src/Components/Pendingpage/Modal.js
+++ src/Components/Pendingpage/Modal.js
@@ -2,10 +2,16 @@ import React from 'react'
 import './Modal.scss'
 import Model1 from './Modal1.png'
 
-const Modal = () => {
+
+const Modal = ({setshowmodal,showmodal}) => {
+    const handleCross=()=>{
+        setshowmodal(false)
+
+    }
   return (
     <div className="main-component">
     <div className="simple-component">
+        <button className='btncross' onClick={handleCross} >X</button>
         <img className='imgmodal' src={Model1}></img>
         <p>Are you sure you want to delete this Outlet ?</p>
     <div className='btndivpending'>
--- src/Components/Pendingpage/ThreeDots.js
+++ src/Components/Pendingpage/ThreeDots.js
@@ -4,11 +4,18 @@ import morevertical from "../../assets/images/more-vertical.jpg"
 import { LuCopyPlus } from "react-icons/lu";
 import { FaRegEdit } from "react-icons/fa";
 import { ImBin } from "react-icons/im";
+import Modal from './Modal';
 
 
 
 const ThreeDots = () => {
     const[threedots,setThreeDots]=useState(false)
+    const[showmodal,setshowmodal]=useState(false)
+    const handleDelete=()=>{
+      setshowmodal(!showmodal)
+
+
+    }
   return (
     <>
     {threedots?(
@@ -16,7 +23,8 @@ const ThreeDots = () => {
         <LuCopyPlus className='copy' />
     <FaRegEdit className='copy'/>
     
-    <ImBin className='copy' />
+    <ImBin className='copy' onClick={handleDelete} />
+    {showmodal?<Modal setshowmodal={setshowmodal} showmodal={showmodal}  />:""}
     </div>
 ):""}
     <button onClick={()=>setThreeDots(!threedots)} className='dots'><img src={morevertical}></img></button>
